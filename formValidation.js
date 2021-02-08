de45a6771028b09cc7e6de9abf5423c89c7dfc29
Maya Mircheva <may4eto@hotmail.com>
2021-02-08 12:54:02 +0100

    deleted scripts

--- formValidation.js
+++
@@ -1,8 +0,0 @@
-function checkInput() {
-        var x = document.forms["checkout"]["email"].value;
-        if (x == null || x == "") {
-            alert("Name must be filled out");
-            return false;
-        }
-    }
-  }
